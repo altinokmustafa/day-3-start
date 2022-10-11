d30eb5b9a0d7fd7f7e955f66d728a104e437df92
Mustafa Altinok <64875678+altinokmustafa@users.noreply.github.com>
2022-10-11 18:29:36 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,29 @@
+
+#print("Welcome to the rollercoaster!")
+#height = int(input("What is your height in cm? "))
+
+#Control Flow with if / else and Conditional Operators
+
+#water_level = 50
+#if water_level > 80:
+  #print("Drain water")
+#else:
+  #print("Continue")
+
+#print("Welcome to the rollercoaster!")
+#height = int(input("What is your height in cm? "))
+
+#if height >= 120:
+#          >
+#          <
+#          <=
+#          ==
+#print ("You can ride the rollercoaster!")
+#else:
+#  print("Sorry, you have to grow taller before you can ride.")
+
+
+
+
+
+
